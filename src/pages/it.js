90e279fd94facc3bc5f312a58a4edc0e360c0cbd
kaleckh <69163050+kaleckh@users.noreply.github.com>
2023-04-20 10:58:19 -0600

    it issues

--- src/pages/it.js
+++ src/pages/it.js
@@ -10,7 +10,7 @@ import styles from '../styles/it.module.css';
 import Footer from './Footer';
 import TawkMessengerReact from "@tawk.to/tawk-messenger-react";
 
-const it = (props) => {
+const It = (props) => {
 	const [ quoteToggle, setQuoteToggle ] = useState(true);
 	const [ buttonToggle, setButtonToggle ] = useState(true);
 
@@ -150,4 +150,4 @@ const it = (props) => {
 	);
 };
 
-export default it;
+export default It;
